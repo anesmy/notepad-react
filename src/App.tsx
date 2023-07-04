aed2c30cf7b62865f69ac64f4e319ff32223b9db
Nesmy <anastasiia.mudra.kn.2021@lpnu.ua>
2023-07-04 12:44:13 +0300

    Create a NotePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
+import NotePage from "./components/NotePage";
 
 function App() {
   return (
     <div className="container mt-3">
       <Routes>
-        {/* <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/gamepage" element={<GamePage />} /> */}
+        <Route path="/" element={<NotePage />} />
+        <Route path="/login" element={<NotePage />} />
+        <Route path="/notepad" element={<NotePage />} />
       </Routes>
     </div>
   );
---
+++ src/components/NotePage.tsx
@@ -0,0 +1,63 @@
+import { Box, Grid, GridItem, Show } from "@chakra-ui/react";
+import { useState } from "react";
+import { Helmet } from "react-helmet";
+import NavBar from "./header/NavBar";
+import NoteHeading from "./NoteHeading";
+import NotesList from "./notesList/NotesList";
+import { Note } from "../hooks/useNotes";
+import NoteGrid from "./NoteGrid";
+
+export interface NoteQuery {
+  searchText: string;
+  note: Note;
+}
+
+function NotePage() {
+  const [noteQuery, setNoteQuery] = useState<NoteQuery>({} as NoteQuery);
+
+  return (
+    <>
+      <Helmet>
+        <meta
+          name="description"
+          content="Create your notes conveniently and beautifully"
+        />
+      </Helmet>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        templateColumns={{
+          base: "1fr",
+          lg: "215px 1fr",
+        }}
+      >
+        <GridItem area="nav">
+          <NavBar
+            onSearch={(searchText) =>
+              setNoteQuery({ ...noteQuery, searchText })
+            }
+          />
+        </GridItem>
+        <Show above="lg">
+          <GridItem area="aside" padding={3.5}>
+            <NotesList
+              selectedNote={noteQuery.note}
+              onSelectNote={(note) => setNoteQuery({ ...noteQuery, note })}
+              noteQuery={noteQuery}
+            />
+          </GridItem>
+        </Show>
+        <GridItem area="main">
+          <Box paddingLeft={2}>
+            <NoteHeading noteQuery={noteQuery} />
+          </Box>
+          <NoteGrid noteQuery={noteQuery} />
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default NotePage;
